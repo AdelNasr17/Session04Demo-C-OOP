8b142e873c746fca0c6d8582f2c5d3c69764f159
adelnasr17102002@outlook.com <adelnasr17102002@outlook.com>
2025-03-13 21:38:12 +0200

    Add project files.

---
+++ Session04Demo C# OOP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Session04Demo_C__OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
